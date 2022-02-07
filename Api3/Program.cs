4af3ec0c8dbc00c58b7e3a2009451fec8d114537
omid salangi <omidsalangi@outlook.com>
2022-02-07 13:22:41 +0000

    add some without problem

--- Api3/Program.cs
+++ Api3/Program.cs
@@ -1,6 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using NLog;
+var logger = NLog.LogManager.Setup().LoadConfigurationFromFile("/nlog.config").GetCurrentClassLogger();
+logger.Debug("init main");
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+    var app = builder.Build();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+   NLog.LogManager.Shutdown();
+}
